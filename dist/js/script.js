7dfa77058ac4de9a9c55c72f4794a20ff81859d4
Muhammad Riduan <inidipuy@gmail.com>
2023-05-30 20:41:17 +0800

    add new featured

--- dist/js/script.js
+++ dist/js/script.js
@@ -55,7 +55,16 @@ const html = document.querySelector('html');
 darkToggle.addEventListener('click', function() {
     if (darkToggle.checked) {
         html.classList.add('dark');
+        localStorage.theme = 'dark';
     } else {
         html.classList.remove('dark');
+        localStorage.theme = 'light';
     }
 })
+
+// membuat toggle menyesuaikan tema web
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+  }
