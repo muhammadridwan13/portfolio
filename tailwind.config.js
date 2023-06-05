18578708a0cd42f8b8ff2f468b9ad952740ec211
Muhammad Riduan <inidipuy@gmail.com>
2023-06-06 01:08:22 +0800

    clearing screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       },
       screens: {
         '2xl': '1320px',
-        'mini': { 'raw': '(max-width: 340px)' },
+        'mini': { 'raw': '(max-width: 365px)' },
       }
     },
   },
